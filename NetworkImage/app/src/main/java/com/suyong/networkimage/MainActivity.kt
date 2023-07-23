36ad2728d47f2765b85a45c631263e41ae862d1d
suyong-park <spdlqjfire@naver.com>
2023-07-23 23:27:53 +0900

    compose learning NetworkImage

--- NetworkImage/app/src/main/java/com/suyong/networkimage/MainActivity.kt
+++ NetworkImage/app/src/main/java/com/suyong/networkimage/MainActivity.kt
@@ -3,12 +3,16 @@ package com.suyong.networkimage
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import coil.compose.AsyncImage
+import coil.compose.rememberImagePainter
 import com.suyong.networkimage.ui.theme.NetworkImageTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,7 +22,7 @@ class MainActivity : ComponentActivity() {
             NetworkImageTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
+                    modifier = Modifier.wrapContentSize(),
                     color = MaterialTheme.colors.background
                 ) {
                     NetworkImageSample()
@@ -30,7 +34,19 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun NetworkImageSample() {
+    Column {
+        val painter = rememberImagePainter(data = "https://search.pstatic.net/sunny/?src=https%3A%2F%2Fthumbs.dreamstime.com%2Fz%2Fflying-birds-dusk-japanese-town-walk-alone-river-bathe-watch-fly-sky-be-stained-golden-feel-particularly-relaxed-115642823.jpg&type=sc960_832")
 
+        Image(
+            painter = painter,
+            contentDescription = "테스트이미지"
+        )
+
+        AsyncImage(
+            model = "https://search.pstatic.net/sunny/?src=https%3A%2F%2Fthumbs.dreamstime.com%2Fz%2Fflying-birds-dusk-japanese-town-walk-alone-river-bathe-watch-fly-sky-be-stained-golden-feel-particularly-relaxed-115642823.jpg&type=sc960_832",
+            contentDescription = "테스트이미지2"
+        )
+    }
 }
 
 @Preview(showBackground = true)
