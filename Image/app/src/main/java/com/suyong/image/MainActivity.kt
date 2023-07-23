f750030063dd9836601fb3d5d47998bda6f5a1be
suyong-park <spdlqjfire@naver.com>
2023-07-23 22:37:39 +0900

    compose learning Image

--- Image/app/src/main/java/com/suyong/image/MainActivity.kt
+++ Image/app/src/main/java/com/suyong/image/MainActivity.kt
@@ -3,11 +3,16 @@ package com.suyong.image
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Settings
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.suyong.image.ui.theme.ImageTheme
 
@@ -18,7 +23,7 @@ class MainActivity : ComponentActivity() {
             ImageTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
+                    modifier = Modifier.wrapContentSize(),
                     color = MaterialTheme.colors.background
                 ) {
                     ImageExample()
@@ -30,7 +35,17 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun ImageExample() {
+    Column() {
+        Image(
+            painter = painterResource(id = R.drawable.test),
+            contentDescription = "태극기"
+        )
 
+        Image(
+            imageVector = Icons.Filled.Settings,
+            contentDescription = "설정"
+        )
+    }
 }
 
 @Preview(showBackground = true)
