b755c9f07bef7711bd40d817292c919271c3717a
suyong-park <spdlqjfire@naver.com>
2023-07-24 19:54:19 +0900

    compose learning TopAppBar

--- TopAppBar/app/src/main/java/com/suyong/topappbar/MainActivity.kt
+++ TopAppBar/app/src/main/java/com/suyong/topappbar/MainActivity.kt
@@ -3,9 +3,19 @@ package com.suyong.topappbar
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountBox
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.filled.Settings
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -21,7 +31,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    TopAppBarExample()
+                    TopAppBarExample("TopAppBar")
                 }
             }
         }
@@ -29,14 +39,95 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun TopAppBarExample() {
+fun TopAppBarExample(name: String) {
+    Column() {
+//        TopAppBar(title = { Text(text = name) })
+//        TopAppBar(
+//            title = { Text(text = name) },
+//            navigationIcon = {
+//                IconButton(onClick = { }) {
+//                    Icon(
+//                        imageVector = Icons.Filled.ArrowBack,
+//                        contentDescription = "Arrow Back"
+//                    )
+//                }
+//            }
+//        )
+        TopAppBar(
+            title = { Text(text = name) },
+            navigationIcon = {
+                IconButton(onClick = { }) {
+                    Icon(
+                        imageVector = Icons.Filled.ArrowBack,
+                        contentDescription = "Arrow Back"
+                    )
+                }
+            },
+            actions = {
+                IconButton(onClick = { }) {
+                    Icon(
+                        imageVector = Icons.Filled.Search,
+                        contentDescription = "Search"
+                    )
+                }
 
+                IconButton(onClick = { }) {
+                    Icon(
+                        imageVector = Icons.Filled.Settings,
+                        contentDescription = "Settings"
+                    )
+                }
+
+                IconButton(onClick = { }) {
+                    Icon(
+                        imageVector = Icons.Filled.AccountBox,
+                        contentDescription = "Account"
+                    )
+                }
+            }
+        )
+
+        // 처음부터 RowScope 을 갖는 TopAppBar 가 존재한다.
+        TopAppBar {
+            IconButton(onClick = { }) {
+                Icon(
+                    imageVector = Icons.Filled.ArrowBack,
+                    contentDescription = "Arrow Back"
+                )
+            }
+            // weight 가 해당 scope 에서 1개만 있는 경우, 가용 가능한 사이즈를 모두 가져가게 된다.
+            Text(text = name, modifier = Modifier.weight(1f))
+
+            IconButton(onClick = { }) {
+                Icon(
+                    imageVector = Icons.Filled.Search,
+                    contentDescription = "Search"
+                )
+            }
+
+            IconButton(onClick = { }) {
+                Icon(
+                    imageVector = Icons.Filled.Settings,
+                    contentDescription = "Settings"
+                )
+            }
+
+            IconButton(onClick = { }) {
+                Icon(
+                    imageVector = Icons.Filled.AccountBox,
+                    contentDescription = "Account"
+                )
+            }
+        }
+
+        Text(text = "Hello $name")
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun TopAppBarPreview() {
     TopAppBarTheme {
-        TopAppBarExample()
+        TopAppBarExample("TopAppBar")
     }
 }
